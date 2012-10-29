88eda09dcde1986fa0fead1b620cb93b41f2e7ef
Ben Rudolph <rudolphben@gmail.com>
2012-10-29 03:22:29 -0700

    All locations mapped and responding to events

--- public/js/map.js
+++ public/js/map.js
@@ -20,11 +20,9 @@ var Map = function(selector, options, countries) {
   })
 
   //this.createCountryMarkers(countries)
-  this.placeMarkers()
   this.barGraph = null
-  this.index = 0
-  this.loadAddresses(addresses)
-  this.locations = []
+  this.locations = locations
+  this.placeMarkers()
 }
 
 Map.COUNTRY_MAP = {
@@ -39,74 +37,19 @@ Map.COUNTRY_MAP = {
 
 Map.method("placeMarkers", function() {
   var marker;
-  for (var country in Map.COUNTRY_MAP) {
+  for (var key in this.locations) {
     marker = new google.maps.Marker({
-      position: Map.COUNTRY_MAP[country],
+      position: this.locations[key],
       map: this.gMap,
-      title: country
+      title: key
     })
     this.markers.push(marker)
-    google.maps.event.addListener(marker, "click", function() {
-      alert(marker.title)
-    })
+    google.maps.event.addListener(marker, "click", (function(marker, key) {
+      return function() {
+        alert(marker.title)
+      }
+    })(marker, key))
   }
-})
-
-Map.method("loadAddress", function(a) {
-  if (this.index >= a.length) {
-    $("body").html("")
-    for (var idx in this.locations) {
-      $("body").append(this.locations[idx])
-    }
-    clearInterval(this.iId)
-    return
-  }
-  this.geocoder.geocode({ "address": a[this.index] }, function (results, status) {
-        var loc;
-        console.log(a[this.index])
-        if (status == google.maps.GeocoderStatus.OK) {
-            loc = results[0].geometry.location
-            console.log("lat: " + loc.lat())
-            console.log("lng: " + loc.lng())
-            this.locations.push('"' + a[this.index] + '": new google.maps.LatLng(' + loc.lat() + ', ' + loc.lng() + '),<br />')
-        } else {
-          console.log(status)
-        }
-        if (status !== "OVER_QUERY_LIMIT")
-          this.index += 1
-
-      }.bind(this))
-
-})
-
-Map.method("loadAddresses", function(a) {
-  this.iId = setInterval(this.loadAddress.bind(this), 800, a)
-})
-
-Map.method("createCountryMarkers", function(countries) {
-  countries.forEach(function(country) {
-    if (country in Map.COUNTRY_MAP)
-      return
-    this.geocoder.geocode({ "address": country }, function (results, status) {
-        var loc;
-        if (status == google.maps.GeocoderStatus.OK) {
-            loc = results[0].geometry.location
-            console.log(country)
-            console.log("lat: " + loc.lat())
-            console.log("lng: " + loc.lng())
-        } else {
-          console.log(status)
-          return
-        }
-
-        this.markers.push(new google.maps.Marker({
-          position: loc,
-          map: this.gMap,
-          title: country
-        }))
-
-      }.bind(this))
-    }.bind(this))
 })
 
 
